f48f9967fad062129561e1af667444246b4c089c
Kiran Baktha <sundarambaktha@hotmail.com>
2018-06-17 14:06:29 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# Domain Availability Check
+
+from nltk import FreqDist
+from nltk.corpus import brown
+import requests
+import json
+import re
+
+words = []
+frequency_list = FreqDist(i.lower() for i in brown.words())
+for pair in frequency_list.most_common():
+    if len(pair[0]) >= 7 and re.fullmatch('[a-z]+',pair[0]) is not None: # Simple regex match to avoid special characters
+        words.append(pair[0])
+    if len(words)==500:
+        break
+
+
+def get_availability(words):
+    result = {}
+    for i in range(len(words)):
+        try:
+            domain = words[i] + '.com'
+            response = requests.get("https://jsonwhois.com/api/v1/whois",headers={
+                                "Accept": "application/json",
+                                "Authorization": "Token token=<your token>"},
+                        params={"domain": domain})
+            content = response.content
+            content = content.decode('utf-8')
+            json_content = json.loads(content)
+            result[domain] = json_content['available?']
+        except:
+            print(domain)
+    return result
+    
+    
+    
+availabilities = get_availability(words)
+
+# Print the available domains
+for domain in availabilities:
+    if availabilities[domain]:
+        print(domain)
+
